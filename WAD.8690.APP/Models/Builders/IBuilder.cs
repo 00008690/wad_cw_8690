5fb23f6a900bdb550df8c268719aba6b45a21339
Otabek Rasulov <beeek8446@gmail.com>
2021-04-20 22:56:24 +0400

    react spa

---
+++ WAD.8690.APP/Models/Builders/IBuilder.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WAD._8690.APP.Models.Builders
+{
+    public interface IBuilder
+    {
+        void SetPrice();
+        void SetDeliveredTime();
+        void SetName();
+        void SetAmount();
+        void SetBasePrice();
+        void SetSoldPrice();
+        void SetExpireDate();
+        void SetBuyer();
+        void SetDiscount();
+    }
+}
---
+++ WAD.8690.APP/Models/List.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace WAD._8690.APP.Models
+{
+    public class List
+    {
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        [DisplayName("List name")]
+        public string ListName { get; set; }
+        [Required]
+        [DisplayName("Created date")]
+        public DateTime CreatedDate { get; set; }
+
+        //navigation properties
+
+        public List<Task> Tasks { get; set; }
+    }
+}
